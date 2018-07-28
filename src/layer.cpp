b91fb7add426eab632ea04bda5421823a96d2c1f
void <thevoidf@gmail.com>
2018-07-28 14:35:12 +0400

    fixed unisnged int comparison warrning

--- src/layer.cpp
+++ src/layer.cpp
@@ -23,7 +23,7 @@ namespace lowg {
 
 	void Layer::remove(void *ptr)
 	{
-		for (int i = 0; i < renderables.size(); i++) {
+		for (unsigned int i = 0; i < renderables.size(); i++) {
 			if (renderables[i] == ptr)
 				renderables.erase(renderables.begin() + i);
 		}
@@ -34,14 +34,13 @@ namespace lowg {
 		renderables.erase(renderables.begin() + index);
 	}
 
-
 	void Layer::render()
 	{
 		shader->enable();
 
 		((BatchRenderer2D*) renderer)->begin();
 		for (const Renderable2D* renderable : renderables)
-				renderable->submit(((BatchRenderer2D*) renderer));
+			renderable->submit(((BatchRenderer2D*) renderer));
 		((BatchRenderer2D*) renderer)->end();
 
 		renderer->flush();
